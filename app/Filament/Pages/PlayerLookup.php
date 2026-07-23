32b40cd5a5f92e4e2c0c9768de0811b546105e2e
liming <liming@users.noreply.github.com>
2026-07-23 23:17:52 +0800

    玩家状态判断

--- app/Filament/Pages/PlayerLookup.php
+++ app/Filament/Pages/PlayerLookup.php
@@ -243,7 +243,7 @@ class PlayerLookup extends Page
 
     public function isBanned(array $user): bool
     {
-        return (int) Arr::get($user, 'banDay', 0) > 20991201;
+        return (int) Arr::get($user, 'banDay', 0) > 1;
     }
 
     public function banStatus(array $user): string
